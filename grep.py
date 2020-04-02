decb576e7c28821e7f179ec675e5233df14c5969
Petr Louda <loudapet@jupyter.korpus.cz>
2020-04-02 15:39:27 +0200

    Verze s logging před formální úpravou, bez argparse

--- grep.py
+++ grep.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-# docstring = dokumentační řetězec; složí se do kouzelné proměnné __doc__
 """Usage: grep.py PATTERN FILE [FILE...]
-
-Print lines from each FILE matching regular expression PATTERN. Print lines from each FILE matching regular expression PATTERN. Pass `-` for
+Print lines from each FILE matching regular expression PATTERN. Pass `-` for
 standard input.
-
 """
-#program env vyhledá preferovanou verzi programu, přes který se skript spustí (lokace pomocí "which env")
-# příkaz chmod +x grep.py
+
 import sys
 import fileinput as fi
 import logging as log
-import regex as re
+import re
 
 # modul logging umožňuje rozčlenit zprávy uživateli do různých úrovní
 # důležitosti. v pořadí od nejméně důležité to jsou DEBUG, INFO, WARNING, ERROR
@@ -19,13 +15,14 @@ import regex as re
 # pokud chceme jít níž, musíme si to nakonfigurovat:
 log.basicConfig(level=log.INFO)
 
+
 def grep(pattern, lines):
     """Print lines matching pattern."""
     for line in lines:
-        line = line.strip()
         if re.search(pattern, line):
-            print(line)
-            
+            print(line, end="")
+
+
 def parse_argv(argv):
     """Parse script arguments."""
     # pokud dostaneme míň než dva parametry, ručně vyvoláme ValueError
@@ -36,9 +33,11 @@ def parse_argv(argv):
     pattern, paths = argv[0], argv[1:]
     return pattern, paths
 
-def main():
+
+def main(argv):
+    """Main entry point of script."""
     try:
-        pattern, paths = parse_argv(sys.argv)
+        pattern, paths = parse_argv(argv)
     except ValueError:
         log.critical(__doc__.strip())
         sys.exit(1)
@@ -49,6 +48,7 @@ def main():
         log.critical(__doc__.strip())
         log.critical(err)
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
